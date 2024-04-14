99e2d9c0e8c6e5cda640e3148d5e6505b76d6028
ShyamaAgrawal <shyama.agrawal162@gmail.com>
2024-04-14 17:12:41 +0530

    HomePage

--- src/Components/Repo.jsx
+++ src/Components/Repo.jsx
@@ -1,7 +1,10 @@
-import React,{useState, useEffect} from 'react'
+import  { useState, useEffect } from 'react'
 import avatar from '.././Images/bg.png'
 import Repositories from "../../public/list.json"
 import './CSS/Repo.css'
+import gmail from '../Images/gmail.png'
+import location from '../Images/location.png'
+import followers from '../Images/followers.png'
 //comment
 function Repo() {
     const [repo, setRepo] = useState(Repositories);
@@ -24,82 +27,91 @@ function Repo() {
         if (sortBy === 'name') {
             return a.name.localeCompare(b.name);
         } else if (sortBy === 'forks') {
-            return a.forks - b.forks;
+            return b.forks - a.forks;
         } else if (sortBy === 'star') {
-            return a.star - b.star;
+            return b.star - a.star;
         } else if (sortBy === 'open_issues') {
-            return a.open_issues - b.open_issues;
+            return b.open_issues - a.open_issues;
         }
         return 0;
     });
 
-  return (
-    <>
+    return (
+        <>
             <div className='container'>
                 <nav>
-                    <div className="leftn">
-                        <h1>UserNAme</h1>
-                    </div>
-                    <div className="rightn">
-                        
-                            <a href="">HOME</a>
-                        
-                    </div>
-                    
+                    <div style={{marginLeft: '10%',color:'#10147d'}}><h1 className='logo' >UserName</h1></div>
+                    <div style={{marginRight: '10%',backgroundColor:'#10147d',padding:'8px 16px',borderRadius: '10px',}} className='home'><a style={{color:'white'}} href="">HOME</a></div>
+
                 </nav>
-                <section>
-                <div className="left">
-                    <img src={avatar} alt="" />
-                    <h1>ABC XYZ </h1>
-                    <h3>abc@gmail.com</h3>
-                    <h4>Total Repositories: {repo.length}</h4>
-                </div>
-                <div className="right">
-                    <div className="func">
-                        <div className="sort">
-                            <label htmlFor="sortBy">Sort by:</label>
-                            <select id="sortBy" value={sortBy} onChange={handleSortChange}>
-                                <option value="name">Name</option>
-                                <option value="forks">Forks</option>
-                                <option value="star">Stars</option>
-                                <option value="open_issues">Open Issues</option>
-                            </select>
+                <section style={{ marginLeft: '10%', marginRight: '10%', height: '90vh' }}>
+                    <div className="left">
+                        <img className='avatar' src={avatar} alt="" />
+                        <h1 style={{color:'beige'}}>Kashish Bansal</h1>
+                        <h3 style={{ lineHeight: 1.5 }}>Pre-final year | Computer Science and Engineering | GLA University</h3>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <img src={gmail} style={{ width: '20px', height: '20px', marginRight: '5px' }} alt="" />
+                            <h3>shyama.agrawal@gmail.com</h3>
                         </div>
-                        <div className="search">
-                            <label htmlFor="search">Search:</label>
-                            <input
-                                type="text"
-                                placeholder="Search by name..."
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                            />
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <img src={location} style={{ width: '20px', height: '20px', marginRight: '5px' }} alt="" />
+                            <p>New Delhi</p>
                         </div>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <img src={followers} style={{ width: '20px', height: '20px', marginRight: '5px' }} alt="" />
+                            <p style={{ marginRight: '10px', }}>100 followers</p>
+                            <p> * 50 following</p>
+                        </div>
+
+
                     </div>
-                    <div className="box">
-                        <div className='repo-cards-wrapper'>
-                            <div className="repo-cards">
-                                {sortedRepo.map(rep => (
-                                    <div key={rep.id} className="repo-card" >
-                                        <div className="content">
-                                            <span >{rep.name}</span>
-                                            <p className='des'>{rep.desc}</p>
+                    <div className="right">
+                        <h1 style={{ color: '#10147d', display: 'inline-block', }}>Your Repositories</h1> <div style={{ display: 'inline-block', border: '1px solid gray', color: '#10147d', padding: '3px', backgroundColor: '#eeeef6', width: '20px', height: '20px', textAlign: 'center', borderRadius: '10px', fontWeight: 'bold', alignSelf: 'center' }}>{repo.length}</div>
+                        <div className="func">
+                            <div className="sort">
+                                <label htmlFor="sortBy">Sort by:</label>
+                                <select id="sortBy" value={sortBy} onChange={handleSortChange}>
+                                    <option value="name">Name</option>
+                                    <option value="forks">Forks</option>
+                                    <option value="star">Stars</option>
+                                    <option value="open_issues">Open Issues</option>
+                                </select>
+                            </div>
+                            <div className="search">
+                                <label htmlFor="search">Search:</label>
+                                <input
+                                    type="text"
+                                    placeholder="Search by name..."
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="box">
+                            <div className='repo-cards-wrapper'>
+                                <div className="repo-cards">
+                                    {sortedRepo.map(rep => (
+                                        <div key={rep.id} className="repo-card" >
+                                            <div className="content">
+                                                <span >{rep.name}</span>
+                                                <p className='des'>{rep.desc}</p>
+                                            </div>
+                                            <div className="meta">
+                                                <p><i className="ri-git-fork-line"></i>{rep.forks}</p>
+                                                <p><i className="ri-star-line"></i>{rep.star}</p>
+                                                <p>Open Issue: {rep.open_issues}</p>
+                                            </div>
                                         </div>
-                                        <div className="meta">
-                                            <p><i class="ri-git-fork-line"></i>{rep.forks}</p>
-                                            <p><i class="ri-star-line"></i>{rep.star}</p>
-                                            <p>Open Inssue: {rep.open_issues}</p>
-                                        </div>
-                                    </div>
-                                ))}
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 </section>
             </div>
-      
-    </>
-  )
+
+        </>
+    )
 }
 
 export default Repo
