87919e6a34cc4c91a6444d76ad242e5fb29d846e
Jayant Sharma <105975938+JayantSharma17@users.noreply.github.com>
2024-04-14 13:50:02 +0530

    react-router-dom implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-// import React from 'react'
 import './App.css'
 import DashBoard from './Components/DashBoard'
+import { Route,Routes } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
-      <DashBoard />
+      <Routes>
+        <Route exact path='/' Component={DashBoard} />
+        {/* <Route exact path='/home' Component={HomePage} /> */}
+      </Routes>
     </div>
   )
 }
