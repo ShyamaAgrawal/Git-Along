c03cf724e04c6ec6d8f1c326b05fc270b3674eb4
KHUSHI-AGRAWAL-1 <khushi.agrawal_cs21@gla.ac.in>
2024-04-14 14:18:56 +0530

    repo add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import DashBoard from './Components/DashBoard'
 import { Route,Routes } from 'react-router-dom';
+import Repo from './Components/Repo';
 
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
     <div>
       <Routes>
         <Route exact path='/' Component={DashBoard} />
-        {/* <Route exact path='/home' Component={HomePage} /> */}
+        <Route exact path='/home' Component={Repo} />
+        
       </Routes>
     </div>
   )
